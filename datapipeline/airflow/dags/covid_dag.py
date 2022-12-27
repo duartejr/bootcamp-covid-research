c3c4cb4ae585fe9d697fa46218b982925638979d
Duarte Junior <duarte.jr105@gmail.com>
2022-12-27 15:29:39 -0300

    Fix corrigido o operador para transformar os dados de covid

--- datapipeline/airflow/dags/covid_dag.py
+++ datapipeline/airflow/dags/covid_dag.py
@@ -14,20 +14,19 @@ ARGS = {"owner"           : "airflow",
         "start_date"      : days_ago(1)}
 TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.00Z'
 BASE_FOLDER = join(str(Path("/mnt/d/bootcamp-covid")),
-                       "datalake/{stage}/covid_data/{country}/{partition}")
+                       "datalake/{stage}/covid_data/{partition}")
 EXTRACT_DATE = dt.now() - timedelta(days=1)
 PARTITION_FOLDER = f"extract_date={dt.strftime(EXTRACT_DATE, '%Y-%m-%d')}"
-COUNTRIES = ["Spain", "Ecuador", "Chile", "Mexico", "Argentina"]
+COUNTRIES = "Spain,Ecuador,Chile,Mexico,Argentina"
 
 def transform_covid_data(**kwargs):
     src = kwargs["src"]
     dest = kwargs["dest"]
     extract_date = kwargs["extract_date"]
-    country = kwargs["country"]
+    countries = kwargs["countries"]
     subprocess.run(["python",
                     "/mnt/d/bootcamp-covid/datapipeline/covid_data_process/covid_data_transformation.py",
-                    src, dest, extract_date, country])
-    
+                    src, dest, extract_date, countries])
 
 with DAG(dag_id          = "Covid_dag",
          default_args    = ARGS,
@@ -35,7 +34,6 @@ with DAG(dag_id          = "Covid_dag",
     
     covid_operator = CovidOperator(task_id   = "get_covid_data",
                                    file_path = join(BASE_FOLDER.format(stage     = "bronze",
-                                                                       country   = "",
                                                                        partition = PARTITION_FOLDER),
                                                     f"CovidData_{dt.strftime(EXTRACT_DATE, '%Y%m%d')}.csv"),
                                    date      = dt.strftime(EXTRACT_DATE, '%m-%d-%Y'))
@@ -45,12 +43,12 @@ with DAG(dag_id          = "Covid_dag",
             python_callable = transform_covid_data,
             op_args = [],
             op_kwargs = {"src" : BASE_FOLDER.format(stage      = "bronze",
-                                                          country    = country,
-                                                          partition  = PARTITION_FOLDER),
+                                                    partition  = PARTITION_FOLDER),
                          "dest" : BASE_FOLDER.format(stage     = "silver",
-                                                        country   = country,
-                                                        partition = ""),
-                         "process_date" : "{{ ds }}",
-                         "table_name": "tweet_processed"}
+                                                     partition = "time_series"),
+                         "extract_date" : dt.strftime(EXTRACT_DATE, '%Y-%m-%d'),
+                         "countries": COUNTRIES}
     )
+    
+    covid_operator >> covid_transform
 
--- datapipeline/covid_data_process/covid_data_transformation.py
+++ datapipeline/covid_data_process/covid_data_transformation.py
@@ -5,22 +5,20 @@ from pyspark.sql import functions as f
 from datetime import datetime as dt
 
 def select_country(df, country):
-    return df.filter(df.Country_region == country)
+    return df.filter(df.Country_Region == country)
 
-def select_columns(df):
-    df = df.select(f.col("Country_Region"),
-                   f.col("date"),
-                   f.col("Confirmed").cast("double"),
-                   f.col("Deaths").cast("double"),
-                   f.col("Recovered").cast("double"),
-                   f.col("Active").cast("double"),
-                   f.col("Incident_Rate").cast("double"),
-                   f.col("Case_Fatality_Ratio").cast("double"))
+def select_columns(spark, df):
+    df.createOrReplaceTempView("tempView")
+    df = spark.sql("SELECT Country_Region, date, "+\
+                   "CAST(Confirmed AS DOUBLE) Confirmed, "+\
+                   "CAST(Deaths AS DOUBLE) Deaths, "+\
+                   "CAST(Recovered AS DOUBLE) Recovered, "+\
+                   "CAST(Incident_Rate AS DOUBLE) Incident_Rate, "+\
+                   "CAST(Case_Fatality_Ratio AS DOUBLE) Case_Fatality_Ratio "+\
+                   "FROM tempView")
     return df
 
-
 def group_data(spark, df):
-    df.createOrReplaceTempView("tempView")
     df = spark.sql("select date, Country_Region, "+\
                    "sum(Confirmed) as Confirmed, "+\
                    "sum(Deaths) as Deaths, "+\
@@ -28,22 +26,20 @@ def group_data(spark, df):
                    "sum(Active) as Active, "+\
                    "avg(Incident_Rate) as Incident_Rate, "+\
                    "avg(Case_Fatality_Ratio) as Case_Fatality_Ratio "+\
-                   "from try group by date, Country_Region")
+                   "from tempView group by date, Country_Region")
     return df                                           
 
 def save_time_series(df, dest):
-    df.coalesce(1).write("append").parquet(dest)
+    df.coalesce(1).write.mode("append").parquet(dest)
 
 def execute(spark, src, dest, extract_date, country):
-    extract_date = dt.strftime(extract_date, "%Y-%m-%d")
-    in_file = join(src, f"extract_date={extract_date}")
-    out_file = join(dest, "time_series", country)
+    out_file = join(dest, country)
     
     df = spark.read.option("header", True)\
-                   .csv(in_file)
+                   .csv(src)
     
     df = select_country(df, country)
-    df = select_columns(df)
+    df = select_columns(spark, df)
     
     if df.count() > 1:
         df = group_data(spark, df)
@@ -53,14 +49,16 @@ def execute(spark, src, dest, extract_date, country):
 
 if __name__ == "__main__":
     src = sys.argv[1]
-    dest = sys.arg[2]
+    dest = sys.argv[2]
     extract_date = sys.argv[3]
-    country = sys.argv[4]
+    countries = sys.argv[4]
     
     spark = SparkSession\
             .builder\
             .appName("covid_transform")\
             .getOrCreate()
+
+    for country in countries.split(","):
+        execute(spark, src, dest, extract_date, country)
     
-    execute(spark, src, dest, extract_date)
     spark.stop()
