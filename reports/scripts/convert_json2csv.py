1114a76b34a6aeb581dfdc3d8b4d559d2f53ea3b
Duarte Junior <duarte.jr105@gmail.com>
2023-01-09 19:08:30 -0300

    Atualização dos notebooks dos modelos arima

--- reports/scripts/convert_json2csv.py
+++ reports/scripts/convert_json2csv.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 import os
+import csv
 
 spark = SparkSession\
         .builder\
@@ -8,14 +9,16 @@ spark = SparkSession\
 countries = ['AR', 'MX', 'CL', 'EC', 'ES']
 
 for country in countries:
-    src = f'../../datalake/silver/twitter/sentimental_analysis/{country}'
+    src = f'../../datalake/silver/twitter/sentimental_analysis/json/{country}'
     df = spark.read.json(src)
+    df = df.na.drop()
     df = df.toPandas()
-    df = df[['date', 'location', 'texto', 'translation', 'compound',
-             'neg', 'neu', 'pos']]
+    df = df[['date', 'location', 'compound', 'neg', 'neu', 'pos', 'texto', 'translation']]
     src_out = f'../../datalake/silver/twitter/sentimental_analysis/csv/{country}'
     if not os.path.exists(src_out):
         os.makedirs(src_out)
     output_file = f'{src_out}/twitter_sentimental_analysis_{country}.csv'
     print(output_file)
-    df.to_csv(output_file, index=None)
+    df.to_csv(output_file, index=None, quoting=csv.QUOTE_NONNUMERIC)
+
+# spark.exit()
