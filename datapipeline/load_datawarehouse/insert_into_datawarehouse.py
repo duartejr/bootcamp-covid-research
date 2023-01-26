14551434bb23c9eb52f3c5aa8c38b09f8525ec7c
Duarte Junior <duarte.jr105@gmail.com>
2023-01-26 20:56:47 -0300

    Documenting scripts, insert_into_datawarehouse.py

--- datapipeline/load_datawarehouse/insert_into_datawarehouse.py
+++ datapipeline/load_datawarehouse/insert_into_datawarehouse.py
@@ -1,86 +1,145 @@
-from datetime import datetime
+# insert_into_datawarehouse - Script to insert data into the data warehouse.
+#
+# Author: Duarte Junior <duarte.jr105@gmail.com>
+#
+# Licensed to GNU General Public License under a Contributor Agreement.
+import sys
+import pandas as pd
+from airflow.models import Variable
 import mysql.connector as mysql
 from mysql.connector import Error
-import pandas as pd
-import sys
-from airflow.models import Variable
 
-database_user = Variable.get("database_user")
-database_pwd = Variable.get("database_pwd")
-map_countries = {'AR':1, 'CH':2, 'EC':3, 'ES':4, 'MX':5}
+# Control variables
+DATABASE_USER = Variable.get("database_user")
+DATABASE_PSWD = Variable.get("database_pwd")
 
-def insert_into(conn, cursor, sql_query, df):
-	for i, row in df.iterrows():
-		cursor.execute(sql_query, tuple(row))
-		conn.commit()
+def insert_into(conn, cursor, sql_query: str, df: pd.DataFrame):
+    """
+    It executes a SQL query to insert data into the MySQL server.
 
-def fill_table(src, table, sql_query, forecast_date=None, base_folder='arima'):
-	conn = mysql.connect(host = "localhost",
-		                 database = "datawarehouse",
-		                 user = database_user,
-		                 password = database_pwd)
+    Args:
+        conn (connector mysql): The MySQL connector.
+        cursor (cursor mysql): The cursor into the MySQL server.
+        sql_query (str): The query to insert data into the MySQL server.
+        df (pd.DataFrame): Dataframe with the data to inserto into the data warehouse.
+    """
+    for i, row in df.iterrows():
+        cursor.execute(sql_query, tuple(row))
+        conn.commit()
 
-	if table == 'SENTIMENTOS':
-		map_countries = {'AR':1, 'CL':2, 'EC':3, 'ES':4, 'MX':5}
-	else:
-		map_countries = {'AR':1, 'CH':2, 'EC':3, 'ES':4, 'MX':5}
-		
-	try:
-		if conn.is_connected():
-			cursor = conn.cursor()
-			cursor.execute("SELECT database();")
-			record = cursor.fetchone()
-			if not forecast_date:
-				sql = f"DELETE FROM datawarehouse.{table};"
-				cursor.execute(sql)
-				conn.commit()
+def fill_table(src: str, table: str, sql_query: str, process_date=None, base_folder='arima'):
+    """
+    Fill the table in the data warehouse with the provided data.
 
-			if table == "COVID_WORLDOMETER":
-				src_worldometer = f'{src}/{base_folder}_data.csv'
-				df = pd.read_csv(src_worldometer)
-				insert_into(conn, cursor, sql_query, df)
+    Args:
+        src (str): Source directory of the data.
+        table (str): Table name where the data will be inserted.
+        sql_query (str): SQL query to insert data into the data warehouse.
+        process_date (str, optional): The date when the data was collect or processed. Defaults to None.
+        base_folder (str, optional): The folder where the data is stored. Defaults to 'arima'.
+    """
+    # Stabilish the connection with da MySQL server.
+    conn = mysql.connect(host     = "localhost",
+                         database = "datawarehouse",
+                         user     = DATABASE_USER,
+                         password = DATABASE_PSWD)
 
-			else:
-				for country in map_countries:
-					src_country = f'{src}/{base_folder}_{country}.csv'
-					df = pd.read_csv(src_country).dropna()
+    # Creates a dictionaty with the code and the abbreviation of each country.
+    if table == 'SENTIMENTOS':
+        # When the data is provided by Twitter API use this
+        map_countries = {'AR':1, 'CL':2, 'EC':3, 'ES':4, 'MX':5}
+    else:
+        # In other case use this.
+        map_countries = {'AR':1, 'CH':2, 'EC':3, 'ES':4, 'MX':5}
+    
+    # Establish a connection with the MySQL server and execute the query.
+    try:
+        if conn.is_connected(): # If the connection is established.
+            cursor = conn.cursor()
+            cursor.execute("SELECT database();") # Select the database(datawarehouse)
+            record = cursor.fetchone()
+            
+            # If no rocess_date is specified, all existing data in the table
+            # will be deleted before inserting the new data.
+            if not process_date:
+                sql = f"DELETE FROM datawarehouse.{table};"
+                cursor.execute(sql)
+                conn.commit()
 
-					if forecast_date:
-						column = 'forecast_date' if 'forecast_date' in df.columns else 'date'
-						df = df[df[column] == forecast_date]
+            # Insert new data into the COVID_WORLDOMETER table
+            if table == "COVID_WORLDOMETER":
+                src_worldometer = f'{src}/{base_folder}_data.csv' # File with the data.
+                df = pd.read_csv(src_worldometer) # Dataset with the data
+                insert_into(conn, cursor, sql_query, df) # Execute the insertion
 
-					for i, row in df.iterrows():
-						cursor.execute(sql_query, tuple(row))
-						conn.commit()
+            else: # It is execute when the table is not the COVID_WOLDOMETER.
+                for country in map_countries: # Loop to interate each country.
+                    src_country = f'{src}/{base_folder}_{country}.csv' # File with the data.
+                    df = pd.read_csv(src_country).dropna()
+                    
+                    # Select the data in the dataframe according with the process date.
+                    if process_date:
+                        column = 'forecast_date' if 'forecast_date' in df.columns else 'date'
+                        df = df[df[column] == process_date]
 
-	except Error as e:
-		print("Error while connecting to MySQL", e)
+                    # Loop to insert data into the data warehouse.
+                    for i, row in df.iterrows():
+                        cursor.execute(sql_query, tuple(row))
+                        conn.commit()
 
+    except Error as e: # When the connection is not stablished excecute this.
+        print("Error while connecting to MySQL", e)
 
-def execute(src, table, forecast_date=None):
-	if table == 'PREVISAO_CASOS':
-		sql_query = f"INSERT INTO datawarehouse.{table}(`paises_id`, `calendario_data`, `data_prevista`, `casos_previstos`) VALUES (%s, %s, %s, %s)"
-		base_folder = 'arima'
-	if table == "COVID_JHONS_HOPKINS":
-		sql_query = f"INSERT INTO datawarehouse.{table}(`paises_id`, `calendario_data`, `confirmados`, `mortes`, `ativos`, `novos_casos`, `novas_mortes`, `recuperados`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-		base_folder = 'covid_jhons'
-	if table == "SENTIMENTOS":
-		sql_query = f"INSERT INTO datawarehouse.{table}(`calendario_data`, `paises_id`, `tweet`, `negativo`, `positivo`, `neutro`, `composto`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-		base_folder = 'tweet_sentiments'
-	if table == "COVID_WORLDOMETER":
-		columns = '(`paises_id`, `calendario_data`, `populacao`, `total_casos`, `novos_casos`, `total_mortes`, `novas_mortes`, `total_recuperados`, `novos_recuperados`, `casos_ativos`, `casos_criticos`, `total_casos_1Mpop`, `total_mortes_1Mpop`, `total_testes`, `total_testes_1Mpop`, `mortalidade`)'
-		sql_query = f"INSERT INTO datawarehouse.{table}{columns} VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-		base_folder = 'worldometer'
-	
-	fill_table(src, table, sql_query, forecast_date, base_folder)
+def execute(src: str, table: str, process_date=None):
+    """
+    This execute all the process to load and insert the data into thde data warehouse.
+
+    Args:
+        src (str): Source folder with the data to insert into the data warehouse.
+        table (str): Tabla of the data warehouse where the data will be stored.
+        process_date (str, optional): The date when the data was collect or processed. Defaults to None.
+    """
+    if table == 'PREVISAO_CASOS': # Create query to insert data into the table PREVISAO_CASOS
+        sql_query = f"INSERT INTO datawarehouse.{table}(`paises_id`, "+\
+                    "`calendario_data`, `data_prevista`, `casos_previstos`) "+\
+                    "VALUES (%s, %s, %s, %s)"
+        base_folder = 'arima'
+    
+    elif table == "COVID_JHONS_HOPKINS": # Create query to insert data into the table COVID_JHONS_HOPKINS
+        sql_query = f"INSERT INTO datawarehouse.{table}(`paises_id`, "+\
+                    "`calendario_data`, `confirmados`, `mortes`, `ativos`, "+\
+                    "`novos_casos`, `novas_mortes`, `recuperados`) VALUES "+\
+                    "(%s, %s, %s, %s, %s, %s, %s, %s)"
+        base_folder = 'covid_jhons'
+    
+    elif table == "SENTIMENTOS": # Create query to insert data into the table SENTIMENTOS
+        sql_query = f"INSERT INTO datawarehouse.{table}(`calendario_data`, "+\
+                    "`paises_id`, `tweet`, `negativo`, `positivo`, `neutro`, "+\
+                    "`composto`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        base_folder = 'tweet_sentiments'
+    
+    elif table == "COVID_WORLDOMETER": # Create query to insert data into the table COVID_WORLDOMETER
+        columns = "(`paises_id`, `calendario_data`, `populacao`, `total_casos`, "+\
+                  "`novos_casos`, `total_mortes`, `novas_mortes`, `total_recuperados`, "+\
+                  "`novos_recuperados`, `casos_ativos`, `casos_criticos`, "+\
+                  "`total_casos_1Mpop`, `total_mortes_1Mpop`, `total_testes`, "+\
+                  "`total_testes_1Mpop`, `mortalidade`)"
+        sql_query = f"INSERT INTO datawarehouse.{table}{columns} VALUES "+\
+                    "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        base_folder = 'worldometer'
+    
+    # Run the function to insert the data into the datawarehouse
+    fill_table(src, table, sql_query, forecast_date, base_folder)
 
 
 if __name__ == "__main__":
-	src = sys.argv[1]
-	table = sys.argv[2]
-	if len(sys.argv) > 3:
-		forecast_date = sys.argv[3]
-	else:
-		forecast_date = None
+    src = sys.argv[1] # Gets the source folder
+    table = sys.argv[2] # Gets the table name
+    
+    # Check if the process_date is provided
+    if len(sys.argv) > 3:
+        process_date = sys.argv[3]
+    else:
+        process_date = None
 
-	execute(src, table, forecast_date)
+    execute(src, table, process_date) # Calls the execute function
--- datapipeline/load_datawarehouse/insert_into_energia_table.sql
+++
@@ -1,4 +0,0 @@
-COPY datawarehouse."DEMANDA_ENERGIA"(
-	data, pais_id, "demanda_TWh")
-FROM '/opt/bootcamp-covid/datalake/gold/demanda_energia_v2.csv'
-DELIMITER ',';
